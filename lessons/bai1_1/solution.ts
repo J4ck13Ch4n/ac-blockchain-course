78d2a2627ade42010b3e18375d2bc4fae1c81788
Ducky <huuduc6805@gmail.com>
2026-01-30 11:04:02 +0700

    solve bai1_1

--- lessons/bai1_1/solution.ts
+++ lessons/bai1_1/solution.ts
@@ -1,3 +1,4 @@
+import { sha256, toUtf8Bytes } from "ethers";
 
 export type Block = {
   index: number;
@@ -9,5 +10,8 @@ export type Block = {
 
 // ✍️ TODO: Viết hàm tại đây
 export function isValidBlock(block: Block): boolean {
-  return false; // Chỉnh lại logic
+  const data = block.index + block.timestamp + JSON.stringify(block.transactions) + block.previous_hash;
+  const hash = sha256(toUtf8Bytes(data));
+  // slice(2) để bỏ đi 2 ký tự đầu tiên là '0x'
+  return block.current_hash === hash.slice(2);
 }
